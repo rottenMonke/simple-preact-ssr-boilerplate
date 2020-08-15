d87f604844e0b3ed085f9be6afa4026b578d2463
Igor Kazakov <igorkazakovwork@gmail.com>
2020-08-16 01:34:43 +0300

    Update usePageData.js

--- src/lib/hooks/usePageData.js
+++ src/lib/hooks/usePageData.js
@@ -3,7 +3,11 @@ import { useState } from "preact/hooks";
 const usePageData = (pageData) =>
   useState(() => {
     if (pageData) return pageData;
-    if (typeof window !== 'undefined' && window.PAGE_DATA) return window.PAGE_DATA;
+    if (typeof window !== "undefined" && window.PAGE_DATA) {
+      const data = window.PAGE_DATA;
+      delete window.PAGE_DATA;
+      return data;
+    }
     return null;
   });
 
